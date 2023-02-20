9d10b30ee76d382647b076468a5a3322e826e9e2
nataliachen <irenec2025@student.cis.edu.hk>
2023-02-20 09:55:24 +0800

    done

--- src/Scanners.java
+++ src/Scanners.java
@@ -1,30 +1,33 @@
+import java.util.Scanner;
+
 public class Scanners {
 
     public static void main(String[] args) {
 
         // 1.
-
+        Scanner scanner = new Scanner(System.in);
         // 2.
-
+        System.out.println("Please enter your first name:");
         // 3.
-
+        String firstName = scanner.nextLine();
         // 4.
-
+        System.out.println("Please enter your last name:");
         // 5.
-
+        String lastName = scanner.nextLine();
         // 6.
-
+        System.out.println("Please enter your age:");
         // 7.
-
+        int age = scanner.nextInt();
         // 8.
-
+        System.out.println("Please enter your favourite song:");
         // 9.
-
+        String favSong = scanner.nextLine();
         // 10.
-
+        String content = "Hi, my name is " + firstName + lastName + ". I am " + age + " years old and my favourite song is " + favSong;
         // 11.
-
+        System.out.println(content);
         // 12.
+        scanner.close();
 
     }
 }
